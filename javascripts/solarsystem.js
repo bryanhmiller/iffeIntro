f820f919c5dd83723ce719693ee1f1e31c622d86
bryanhmiller <bryankalos@hotmail.com>
2017-02-18 10:47:01 -0600

    Created spaceships.js realMain.js stars.js

---
+++ javascripts/solarsystem.js
@@ -0,0 +1,50 @@
+var SolarSystem = (function(){
+	var planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+	var dwarfPlanets = ["pluto"];
+	var numPlanetsPeopleLandedOn = 0;
+
+	var stars = ["Sun", "Alpha Centauri", "Wolf 359"];
+	var age = 0;
+
+	return {
+		getPlanets: function(){
+			return planets;
+		},
+		getPlanetsLandedPeopleOn: function(){
+			return numPlanetsPeopleLandedOn;
+		},
+		setPlanetsLandedPeopleOn: function(){
+			numPlanetsPeopleLandedOn++;
+		},
+		getDwarfPlanets : function(){
+			return "they are rejects";
+		},
+		setDwarfPlanets : function(lumpOfRock){
+			dwarfPlanets.push(lumpOfRock);
+		},
+		getStars : function(){
+			return stars
+		},
+		setStars : function(){
+			stars.push(newStar);
+		}, 
+		getSolarSystemAge : function(){
+			return age;
+		},
+		setSolarSystemAge : function(){
+			age++;
+		}
+	}
+})();
+
+
+// console.log("Planets: ", SolarSystem.getPlanets());
+// console.log("Planets with people: ", SolarSystem.getPlanetsLandedPeopleOn());
+// SolarSystem.setSpaceships("Voyager 1");
+// SolarSystem.setSpaceships("Voyager 2");
+// SolarSystem.setSpaceships("Gemini");
+// SolarSystem.setSpaceships("Apollo");
+// console.log("Spaceships: ", SolarSystem.getSpaceships());
+
+
+
---
+++ javascripts/spaceships.js
@@ -0,0 +1,14 @@
+var SolarSystem = (function(oldSolarSystem) {
+	var spaceships = [];
+	oldSolarSystem.getSpaceships = function(){
+		return spaceships;
+	};
+	oldSolarSystem.setSpaceships = function(newSpaceship){
+		spaceships.push(newSpaceship);
+	};
+	oldSolarSystem.wreckSpaceships = function(){
+		spaceships.pop();
+	};
+
+	return oldSolarSystem;
+})(SolarSystem || {});
